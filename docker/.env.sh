b9289358311cfdec67b5e00dc7542b88c56d3cf4
ryuji0123 <ryo1011.javelin@gmail.com>
2021-04-07 15:29:26 +0900

    add an environemnt variable

--- docker/.env.sh
+++ docker/.env.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-export IMAGE_NAME=${USER}_python_template
-export CONTAINER_NAME=${USER}_python_template
+export REPOSITORY_NAME=python_template
+export IMAGE_NAME=${USER}_${REPOSITORY_NAME}
+export CONTAINER_NAME=${USER}_${REPOSITORY_NAME}
 export PORT=8888
